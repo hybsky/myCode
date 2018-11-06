07ed7c4efe0a74657e816c674187ac44df6b5c7b
Administrator <1987972332@qq.com>
2018-11-06 21:27:49 +0800

    内部类

---
+++ test/src/InnerClass.java
@@ -0,0 +1,45 @@
+public class InnerClass {
+    public static void main(String[] args) {
+        Benz b = new Benz();
+        b.run("001");
+    }
+}
+
+//内部类在成员变量位置上，编译生成class文件名为:外部类名$内部类名
+//class Benz{
+//    String color;
+//    int lires;
+//    public void run(){
+//        Engine e = new Engine();
+//        e.fire("001");
+//        System.out.println("run");
+//    }
+//    class Engine{
+//        public void fire(String key){
+//            System.out.println(key + "is ready");
+//            System.out.println("fire");
+//        }
+//    }
+//}
+
+//内部类在成员函数（方法）内定义时，编译生成类名:Benz$1Engine.class
+class Benz{
+    String color;
+    //java8不要求对局部变量使用final进行修饰
+    final int lires = 4;
+    public void run(String key){
+        class Engine{
+            public void fire(){
+                //内部类访问局部变量时，不能为局部变量重新赋值
+                //lires = 8;
+                System.out.println(key + "is ready");
+                System.out.println("fire");
+            }
+        }
+        Engine e = new Engine();
+        e.fire();
+        System.out.println("run");
+    }
+
+
+}
