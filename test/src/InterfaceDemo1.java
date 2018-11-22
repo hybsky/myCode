dd5e025ee961dfaee0d0d3c273c4777966d4e290
Administrator <1987972332@qq.com>
2018-11-22 14:26:55 +0800

    在一个接口中，只能定义常量、抽象方法，JDK1.8后可以定义默认的实现方法

--- test/src/InterfaceDemo1.java
+++ test/src/InterfaceDemo1.java
@@ -16,8 +16,11 @@ interface Rich{
     public abstract void rich();
 }
 //接口中方法默认为是抽象的，关键字abstract可以省略
+//在一个接口中，只能定义常量、抽象方法，JDK1.8后可以定义默认的实现方法
 interface Beauty{
-    public void beauty();
+    public default void beauty(){
+        System.out.println("beautiful.....");
+    }
 }
 //接口与接口之间可以多继承的
 interface WRB extends White , Rich , Beauty{
